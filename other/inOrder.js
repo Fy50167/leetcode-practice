d6546e22e3d90aa66b3f5b8fc472fd22a5c394a5
Francis Yang <fy50167@gmail.com>
2024-03-04 15:30:25 -0800

    iterative in order

--- other/inOrder.js
+++ other/inOrder.js
@@ -18,3 +18,29 @@ var inorderTraversal = function(root) {
 
     return output
 };
+
+
+// Iterative
+var inorderTraversal = function(root) {
+
+    const stack = [];
+    const output = [];
+    let current = root;
+    
+    while (current !== null || stack.length > 0) {
+        console.log(current);
+        while (current !== null) {
+            stack.push(current);
+            current = current.left;
+        }
+
+        current = stack.pop();
+
+        output.push(current.val);
+
+        current = current.right;
+
+    }
+
+    return output
+};
